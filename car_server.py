9589acaa068ffbbba24a1220ed1da27a2d8370f9
woflydev <wolfy.coding@gmail.com>
2023-06-23 05:40:54 +1000

    update

--- car_server.py
+++ car_server.py
@@ -13,6 +13,26 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
 
             print(f"\nRECEIVED FROM CLIENT {format(self.client_address[0])}: {(left, right)}")
 
+            data = b'' ### CHANGED
+            payload_size = struct.calcsize("L") ### CHANGED
+            # Retrieve message size
+            while len(data) < payload_size:
+                data += conn.recv(4096)
+
+            packed_msg_size = data[:payload_size]
+            data = data[payload_size:]
+            msg_size = struct.unpack("L", packed_msg_size)[0] ### CHANGED
+
+            # Retrieve all data based on message size
+            while len(data) < msg_size:
+                data += conn.recv(4096)
+
+            frame_data = data[:msg_size]
+            data = data[msg_size:]
+
+            # Extract frame
+            frame = pickle.loads(frame_data)
+
             move(left, right)
 
 if __name__ == "__main__":
