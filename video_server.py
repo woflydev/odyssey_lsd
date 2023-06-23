071316cc99ba86c79888cbd766b33311402d0628
wolfycoding <wolfy.coding@gmail.com>
2023-06-23 18:45:25 +1000

    update

--- video_server.py
+++ video_server.py
@@ -3,33 +3,28 @@ import socket
 import cv2
 import pickle
 
-HOST = ''
+HOST = '0.0.0.0'
 PORT = 8089
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print('Socket created')
-
 s.bind((HOST, PORT))
-print('Socket bind complete')
 s.listen(10)
-print('Socket now listening')
-
 conn, addr = s.accept()
 
-data = b'' ### CHANGED
-payload_size = struct.calcsize("L") ### CHANGED
+data = b''
+payload_size = struct.calcsize("L")
+
+print('VIDEO SERVER INITIALIZED - AWAITING CONNECTIONS AT ' + HOST + ':' + str(PORT))
 
 while True:
-
     # Retrieve message size
     while len(data) < payload_size:
         data += conn.recv(4096)
 
     packed_msg_size = data[:payload_size]
     data = data[payload_size:]
-    msg_size = struct.unpack("L", packed_msg_size)[0] ### CHANGED
+    msg_size = struct.unpack("L", packed_msg_size)[0]
 
-    # Retrieve all data based on message size
     while len(data) < msg_size:
         data += conn.recv(4096)
 
