0961b61b16c60f6d3160d52d5e241f70fdc1a28b
wolfycoding <wolfy.coding@gmail.com>
2023-06-23 18:48:41 +1000

    Update video_client.py

--- video_client.py
+++ video_client.py
@@ -6,19 +6,24 @@ import struct
 
 SERVER_IP = '192.168.0.156'
 
+print("INITIALIZING CAMERA...")
 cap = cv2.VideoCapture(0)
-clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-clientsocket.connect((SERVER_IP, 8089))
+
+try:
+	clientsocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	clientsocket.connect((SERVER_IP, 8089))
+	print("CONNECTION ESTABLISHED!")
+except:
+	print("CONNECTION DROPPED!")
+	exit()
 
 while True:
-	ret,frame=cap.read()
-	# Serialize frame
+	ret, frame = cap.read()
+	
+	if ret == False:
+		print("NO VIDEO FEED FOUND!")
+		break
+	
 	data = pickle.dumps(frame)
-
-	# Send message length first
 	message_size = struct.pack("L", len(data)) ### CHANGED
-
-	# Then data
 	clientsocket.sendall(message_size + data)
-
-print("Server Closed!")
