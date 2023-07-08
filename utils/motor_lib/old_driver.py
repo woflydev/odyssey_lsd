602ef252ed336659deb1a5fe55d17ad22b39884d
woflydev <wolfy.coding@gmail.com>
2023-07-08 09:59:00 +0200

    fixed drivers

--- utils/motor_lib/old_driver.py
+++ utils/motor_lib/old_driver.py
@@ -4,14 +4,14 @@
 import signal
 from time import sleep
 import math
-from telemetrix import telemetrix
+from pyfirmata import Arduino, util, pyfirmata
 import sys 
 
 PWM_FREQ = 3906  # (Hz) max is 1.5 kHz
-MAP_CONST = 1 / 120 * 255  # 1 / 120 to limit speed below 100% duty cycle
+MAP_CONST = 1 / 120   # 1 / 120 to limit speed below 100% duty cycle
 HALF_WIDTH = 0.1          # Half of the width of droid, in metres
 MAX_CENT_ACC = 30000   # Maximum "centripetal acceleration" the robot is allowed to undergo. UNITS ARE DODGY, MUST BE DETERMIEND BY EXPERIMENTATION
-MAX_SPEED = MAP_CONST / 255 * 100  # (percent) max speed of motors
+MAX_SPEED = MAP_CONST * 100  # (percent) max speed of motors
 SERIAL_PORT = '/dev/ttyACM0' # Serial port for Arduino
 
 # Left
@@ -31,37 +31,25 @@ def exit_handler(signal, frame):
     off()
     print("Cleaning up...")  
     print("Done.")  
-    board.shutdown()
+    board.exit()
     sys.exit(0)
 
 signal.signal(signal.SIGINT, exit_handler)
 
-board = telemetrix.Telemetrix()
+board = Arduino(SERIAL_PORT)
 print("Communication Successfully started")
 
-motorLA = 13
-motorLB = 12
-motorRA = 8
-motorRB = 9
+motorLA = board.get_pin('d:13:o')
+motorLB = board.get_pin('d:12:o')
+motorRA = board.get_pin('d:8:o')
+motorRB = board.get_pin('d:9:o')
 
-motorPWMLA = 11
-motorPWMLB = 6
-motorPWMRA = 10
-motorPWMRB = 5
+motorPWMLA = board.get_pin('d:11:p')
+motorPWMLB = board.get_pin('d:6:p')
+motorPWMRA = board.get_pin('d:10:p')
+motorPWMRB = board.get_pin('d:5:p')
 
-Isense = 7
-
-board.set_pin_mode_digital_output(motorLA)
-board.set_pin_mode_digital_output(motorLB)
-board.set_pin_mode_digital_output(motorRA)
-board.set_pin_mode_digital_output(motorRB)
-
-board.set_pin_mode_analog_output(motorPWMLA)
-board.set_pin_mode_analog_output(motorPWMLB)
-board.set_pin_mode_analog_output(motorPWMRA)
-board.set_pin_mode_analog_output(motorPWMRB)
-
-board.set_pin_mode_digital_input(Isense)
+Isense = board.get_pin('d:7:i')
 
 print("Motor driver initialized. \n path: utils\motor_lib\driver.py \n PWM frequency: " + str(PWM_FREQ) + "Hz \n Max speed: " + str(MAX_SPEED) + "%")
 
@@ -69,15 +57,15 @@ print("Motor driver initialized. \n path: utils\motor_lib\driver.py \n PWM frequ
 # off/coast/stop are the same
 def off():
     # Enable pins are low during off() to coast
-    board.analog_write(motorPWMLA,0)
-    board.analog_write(motorPWMLB,0)
-    board.analog_write(motorPWMRA,0)
-    board.analog_write(motorPWMRB,0)
+    motorPWMLA.write(0)
+    motorPWMLB.write(0)
+    motorPWMRA.write(0)
+    motorPWMRB.write(0)
 
-    board.digital_write(motorLA,0)
-    board.digital_write(motorLB,0)
-    board.digital_write(motorRA,0)
-    board.digital_write(motorRB,0)
+    motorLA.write(0)
+    motorLB.write(0)
+    motorRA.write(0)
+    motorRB.write(0)
 
 def stop():
     off()
@@ -87,36 +75,32 @@ def coast():
 
 def brake():
     off()
-    board.digital_write(motorLA,1)
-    board.digital_write(motorLB,1)
-    board.digital_write(motorRA,1)
-    board.digital_write(motorRB,1)
+    motorLA.write(1)
+    motorLB.write(1)
+    motorRA.write(1)
+    motorRB.write(1)
     # Enable pins are high during brake() to brake
-    board.analog_write(motorPWMLA,0)
-    board.analog_write(motorPWMLB,0)
-    board.analog_write(motorPWMRA,0)
-    board.analog_write(motorPWMRB,0)
+    motorPWMLA.write(0)
+    motorPWMRA.write(0)
 
 # brakes after 1.5s of coasting
 def ebrake():
     off()
     sleep(1.5) # sleep(1.5)
-    board.digital_write(motorLA,1)
-    board.digital_write(motorLB,1)
-    board.digital_write(motorRA,1)
-    board.digital_write(motorRB,1)
+    motorLA.write(1)
+    motorLB.write(1)
+    motorRA.write(1)
+    motorRB.write(1)
 
 # forward function
 def fwd(speed, timeout=0):
-    board.analog_write(motorPWMLA,speed * MAP_CONST)
-    board.analog_write(motorPWMLB,0)
-    board.analog_write(motorPWMRA,speed * MAP_CONST)
-    board.analog_write(motorPWMRB,0)
+    motorPWMLA.write(speed * MAP_CONST)
+    motorPWMRA.write(speed * MAP_CONST)
 
-    board.digital_write(motorLA,1)
-    board.digital_write(motorLB,1)
-    board.digital_write(motorRA,1)
-    board.digital_write(motorRB,1)
+    motorLA.write(1)
+    motorLB.write(0)
+    motorRA.write(1)
+    motorRB.write(0)
 
     if timeout > 0:
         sleep(timeout / 1000)
@@ -124,15 +108,13 @@ def fwd(speed, timeout=0):
 
 # reverse function
 def rev(speed, timeout=0):
-    board.analog_write(motorPWMLA,0)
-    board.analog_write(motorPWMLB,speed * MAP_CONST)
-    board.analog_write(motorPWMRA,0)
-    board.analog_write(motorPWMRB,speed * MAP_CONST)
+    motorPWMLB.write(speed * MAP_CONST)
+    motorPWMRB.write(speed * MAP_CONST)
 
-    board.digital_write(motorLA,1)
-    board.digital_write(motorLB,1)
-    board.digital_write(motorRA,1)
-    board.digital_write(motorRB,1)
+    motorLA.write(0)
+    motorLB.write(1)
+    motorRA.write(0)
+    motorRB.write(1)
 
     if timeout > 0:
         sleep(timeout / 1000)
@@ -156,7 +138,7 @@ def turn(speed: float, radius: float, timeout=0):
 def move(RIN, LIN, timeout=0):
     LIN = round(LIN / 5) * 5
     RIN = round(RIN / 5) * 5
-    L = LIN * MAP_CONST  # map values to 0-255
+    L = LIN * MAP_CONST  # map values to 0-1
     R = RIN * MAP_CONST
     #print(L, R)
     if L == 0 and R == 0:
@@ -165,22 +147,22 @@ def move(RIN, LIN, timeout=0):
     else:
         #print(L, R)
         if L > 0:
-            board.analog_write(motorPWMLA,int(abs(L)))
-            board.analog_write(motorPWMLB,0)
+            motorPWMLA.write(abs(L))
+            motorPWMLB.write(0)
         else:
-            board.analog_write(motorPWMLA,0)
-            board.analog_write(motorPWMLB,int(abs(L)))
+            motorPWMLA.write(0)
+            motorPWMLB.write(abs(L))
         if R > 0:
-            board.analog_write(motorPWMRA,int(abs(R)))
-            board.analog_write(motorPWMRB,0)
+            motorPWMRA.write(abs(R))
+            motorPWMRB.write(0)
         else:
-            board.analog_write(motorPWMRA,0)
-            board.analog_write(motorPWMRB,int(abs(R)))
+            motorPWMRA.write(0)
+            motorPWMRB.write(abs(R))
             
-        board.digital_write(motorLA,1)
-        board.digital_write(motorLB,1)
-        board.digital_write(motorRA,1)
-        board.digital_write(motorRB,1)
+        motorLA.write(1)
+        motorLB.write(1)
+        motorRA.write(1)
+        motorRB.write(1)
     if timeout > 0:
         sleep(timeout / 1000)
         off()
