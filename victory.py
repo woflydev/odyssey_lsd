7d49bcfbb09c4d0b23730141f04bc13d15b5909a
woflydev <wolfy.coding@gmail.com>
2023-07-11 09:13:21 +0200

    eoifjeorij

---
+++ victory.py
@@ -0,0 +1,22 @@
+from utils.motor_lib.driver import off, move, brake
+import time
+
+def victory():
+	#Spin
+  for i in range(4):
+    move(-60, 60, 500)
+    move(60, -60, 500)
+    move(-60, -60, 500)
+    move(60, 60, 500)
+  off()
+
+def wheelie():
+  move(60, 60, 500)
+  move(-60, -60, 500)
+  move(60, 60, 600)
+  move(70, 70, 150)
+  move(80, 80, 250)
+  move(90, 90, 300)
+
+wheelie()
+off()
